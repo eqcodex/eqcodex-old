65c38a945ea66dc81b16aabae60738ee229a1782
Xackery Xtal <xackery@gmail.com>
2017-07-07 06:05:51 -0700

    Added initial draft see #2

--- utils/zonedrops_quest/quest.go
+++ utils/zonedrops_quest/quest.go
@@ -4,6 +4,7 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"os"
 	"path/filepath"
@@ -25,10 +26,23 @@ var (
 	lastZone      string
 	questNoSpawns []string
 	insertCount   int
+	recordCount   int
+	dirCount      int
+	insertTotal   int
+	npc_id        int64
+	zone_id       int
 )
 
 type Database struct {
 	instance *sqlx.DB
+}
+
+type QuestData struct {
+	tag         string
+	title       string
+	description string
+	stepnum     int   `db:"quest_stepnum"`
+	questid     int64 `db:"quest_id"`
 }
 
 func main() {
@@ -47,24 +61,30 @@ func main() {
 		log.Fatal(err.Error())
 	}
 
+	fmt.Printf("\033[48;5;7m") //white background
+	fmt.Printf("\033[38;5;0m") //black text
+	fmt.Printf("Scanning quests dir for items to put into zone_drops...")
+	fmt.Printf("\033[48;5;0m\033[38;5;0m\033[0m\n") //restore text
 	//os.Chdir("../../deploy/server/quests")
-	prefixPath = "../../../deploy/server/quests/"
-	prefixPath = "./quests/"
+	//prefixPath = "../../../deploy/server/quests/"
+	prefixPath = "quests/"
 	err = filepath.Walk(prefixPath, visit)
 	if err != nil {
-		log.Fatal("Error filepath", err.Error())
+		log.Fatal("Error filepath: ", err.Error())
 	}
 	fmt.Println("===============")
 	for _, entry := range questNoSpawns {
 		fmt.Println(entry)
 	}
 	fmt.Println("===============")
-	fmt.Println("Created", insertCount, "entries")
+	fmt.Println("Created", insertTotal, " total entries")
 
 }
 
 func visit(path string, f os.FileInfo, err error) error {
-
+	if f == nil {
+		return fmt.Errorf("File does not exist")
+	}
 	if f.IsDir() {
 		//fmt.Println("Dir", path)
 		return nil
@@ -83,11 +103,18 @@ func visit(path string, f os.FileInfo, err error) error {
 	if len(filename) < 1 {
 		return nil
 	}
+
 	if lastZone != dir {
 		lastZone = dir
-		fmt.Println("Zone:", lastZone)
+		recordCount = 0
+		insertCount = 0
+		files, _ := ioutil.ReadDir(prefixPath + dir)
+		dirCount = len(files)
+		//fmt.Println("Zone:", lastZone)
 	}
-
+	recordCount++
+	showPercent(dir, recordCount, dirCount, insertCount, "green")
+	//log.Fatal("Done", prefixPath, " ", dir, " ", recordCount, dirCount)
 	npcname := ""
 	if strings.Contains(filename, ".pl") {
 		npcname = filename[0 : len(filename)-3]
@@ -96,14 +123,14 @@ func visit(path string, f os.FileInfo, err error) error {
 		npcname = filename[0 : len(filename)-4]
 	}
 
-	fmt.Printf("%s, ", filename)
-	zone_id := getZoneByShortname(dir)
+	//fmt.Printf("%s, ", filename)
+	zone_id = getZoneByShortname(dir)
 	if zone_id < 1 {
 		//fmt.Println("Zone skipped", dir)
 		return nil
 	}
 
-	npc_id := getNpcByNameOrId(npcname)
+	npc_id = getNpcByNameOrId(npcname)
 	if npc_id < 1 {
 		//fmt.Println("NPC skipped", filename)
 		questNoSpawns = append(questNoSpawns, fmt.Sprintf("%s/%s", dir, filename))
@@ -115,10 +142,27 @@ func visit(path string, f os.FileInfo, err error) error {
 		log.Fatal(err)
 	}
 	defer file.Close()
-
 	scanner := bufio.NewScanner(file)
+	isIgnore := false
+	questData := &QuestData{}
 	for scanner.Scan() {
 		parse := strings.TrimSpace(strings.Replace(strings.ToLower(scanner.Text()), " ", "", -1))
+
+		if !isIgnore {
+			isIgnore = ignoreParse(parse)
+		}
+
+		if isIgnore {
+			endIgnore := endParse(parse, questData)
+			if !endIgnore {
+				continue
+			}
+		}
+
+		//line found to contain quest data
+		if questParse(parse, questData) {
+			continue
+		}
 
 		isQuestReward := 0
 		isQuestItem := 0
@@ -134,16 +178,17 @@ func visit(path string, f os.FileInfo, err error) error {
 		}
 
 		for _, item_id := range items {
-			fmt.Println("Inserting", zone_id, npc_id, item_id, isQuestReward, isQuestItem, "for", dir, filename)
+			insertCount++
+			insertTotal++
+			//fmt.Println("Inserting", zone_id, npc_id, item_id, isQuestReward, isQuestItem, "for", dir, filename)
 			insertQuery := "REPLACE INTO zone_drops (item_id, npc_id, zone_id, is_quest_reward, is_quest_item) VALUES (?, ?, ?, ?, ?)"
 			stmt, _ := db.instance.Prepare(insertQuery)
 			if _, err = stmt.Exec(item_id, npc_id, zone_id, isQuestReward, isQuestItem); err != nil {
 				log.Fatal(err.Error())
 			}
-			insertCount++
-			if insertCount%1000 == 0 {
-				fmt.Println("Inserted", insertCount, "so far")
-			}
+			//if insertCount%1000 == 0 {
+			//	fmt.Println("Inserted", insertCount, "so far")
+			//}
 		}
 
 		//fmt.Println(scanner.Text())
@@ -154,6 +199,137 @@ func visit(path string, f os.FileInfo, err error) error {
 	}
 	//log.Fatal("Done")
 	return nil
+}
+
+func questParse(parse string, questData *QuestData) bool {
+	var err error
+	if strings.Contains(parse, "#!define") || strings.Contains(parse, "--!define") {
+		qData := strings.Split(parse, "|")
+		if len(qData) != 4 {
+			log.Print("Failed to parse line", parse, "due to split of | being != 4 (", len(qData), ")\n")
+			return true
+		}
+
+		questData.tag = qData[1]
+		if len(questData.tag) > 64 {
+			questData.tag = questData.tag[0:64]
+		}
+		questData.title = qData[2]
+		if len(questData.title) > 128 {
+			questData.title = questData.title[0:128]
+		}
+		questData.description = qData[3]
+		if len(questData.description) > 256 {
+			questData.description = questData.description[0:256]
+		}
+
+		insertQuery := "REPLACE INTO quests (tag, title, description) VALUES (?, ?, ?)"
+		stmt, _ := db.instance.Prepare(insertQuery)
+		if _, err = stmt.Exec(questData.tag, questData.title, questData.description); err != nil {
+			log.Fatal(err.Error())
+		}
+		stmt.Close()
+		if err = db.instance.Get(questData.questid, "SELECT id FROM quests WHERE tag = ?", questData.tag); err != nil {
+			log.Fatal(err.Error())
+		}
+
+		return true
+	}
+
+	if strings.Contains(parse, "#!quest") || strings.Contains(parse, "--!quest") {
+		qData := strings.Split(parse, "|")
+		if len(qData) != 3 {
+			log.Print("Failed to parse line", parse, "due to split of | being != 3 (", len(qData), ")\n")
+			return true
+		}
+		if err = db.instance.Get(&questData, "SELECT * from quests WHERE tag = ?", qData[1]); err != nil {
+			//This is OK, we just insert the data with what we know.
+
+			if result, vErr := db.instance.NamedExec("REPLACE INTO quests (tag) VALUES (:tag)", questData); err != nil {
+				log.Fatal("Failed to insert into quests tag", questData.tag, vErr.Error())
+			} else {
+				questData.questid, _ = result.LastInsertId()
+			}
+		}
+
+	}
+
+	if strings.Contains(parse, "#!item") || strings.Contains(parse, "--!item") {
+		qData := strings.Split(parse, "|")
+		if len(qData) != 2 {
+			log.Print("Failed to parse line", parse, "due to split of | being != 2 (", len(qData), ")\n")
+			return true
+		}
+
+		itemField := qData[1]
+		if len(itemField) < 1 {
+			log.Print("Failed to parse line", parse, "due to len of second element being empty\n")
+			return true
+		}
+		item_id := 0
+		if item_id, err = strconv.Atoi(itemField); err != nil {
+			log.Print("Failed to parse line", parse, "due to non numberic second element\n")
+			return true
+		}
+
+		insertQuery := "REPLACE INTO zone_drops (quest_id, item_id, npc_id, zone_id, quest_stepnum, is_quest_item) VALUES (?, ?, ?, ?, ?, ?)"
+		stmt, _ := db.instance.Prepare(insertQuery)
+		if _, err = stmt.Exec(questData.questid, item_id, npc_id, zone_id, questData.stepnum, 1); err != nil {
+			log.Fatal(err.Error())
+		}
+		stmt.Close()
+		return true
+	}
+
+	if strings.Contains(parse, "#!reward") || strings.Contains(parse, "--!reward") {
+		qData := strings.Split(parse, "|")
+		if len(qData) != 2 {
+			log.Print("Failed to parse line", parse, "due to split of | being != 2 (", len(qData), ")\n")
+			return true
+		}
+
+		itemField := qData[1]
+		if len(itemField) < 1 {
+			log.Print("Failed to parse line", parse, "due to len of second element being empty\n")
+			return true
+		}
+		item_id := 0
+		if item_id, err = strconv.Atoi(itemField); err != nil {
+			log.Print("Failed to parse line", parse, "due to non numberic second element\n")
+			return true
+		}
+
+		insertQuery := "REPLACE INTO zone_drops (quest_id, item_id, npc_id, zone_id, quest_stepnum, is_quest_reward) VALUES (?, ?, ?, ?, ?, ?)"
+		stmt, _ := db.instance.Prepare(insertQuery)
+		if _, err = stmt.Exec(questData.questid, item_id, npc_id, zone_id, questData.stepnum, 1); err != nil {
+			log.Fatal(err.Error())
+		}
+		stmt.Close()
+		return true
+	}
+
+	if strings.Contains(parse, "#!end") || strings.Contains(parse, "--!end") {
+		questData = &QuestData{}
+		return true
+	}
+
+	return false
+}
+
+func getQuestId(shorttag string) {
+
+}
+
+func ignoreParse(parse string) bool {
+	return (strings.Contains(parse, "#!ignore") || strings.Contains(parse, "--!ignore"))
+}
+
+func endParse(parse string, questData *QuestData) bool {
+	if strings.Contains(parse, "#!end") || strings.Contains(parse, "--!end") {
+		questData = &QuestData{}
+		return true
+	}
+	return false
 }
 
 func getQuestReward(parse string) (items []int) {
@@ -277,4 +453,48 @@ func getNpcByNameOrId(npcname string) int64 {
 		return 0
 	}
 	return npcs[0].Id.Int64
+}
+
+func showPercent(message string, cur int, max int, insertCount int, color string) {
+	switch color {
+	case "black":
+		color = "\033[30m"
+	case "maroon":
+		color = "\033[31m"
+	case "green":
+		color = "\033[32m"
+	case "yellow":
+		color = "\033[33m"
+	case "blue":
+		color = "\033[34m"
+	case "purple":
+		color = "\033[35m"
+	case "cyan":
+		color = "\033[36m"
+	case "white":
+		color = "\033[37m"
+	case "gray":
+		color = "\033[38m"
+	case "red":
+		color = "\033[39m"
+	default:
+		color = "\033[0m"
+	}
+
+	dotCount := 30
+	fmt.Printf("\033[60D")
+	val := float64(cur) / float64(max) * float64(dotCount)
+	fmt.Printf("%s%s - [", color, message)
+	for i := 0; i < dotCount; i++ {
+		if int(val) >= i {
+			fmt.Printf(".")
+		} else {
+			fmt.Printf(" ")
+		}
+	}
+	fmt.Printf("]\033[0m")
+	if cur == max {
+		fmt.Printf(" %d zone entries", insertCount)
+		fmt.Printf("\n")
+	}
 }
